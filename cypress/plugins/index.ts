50a9373339e4aaecd9b1d2afb4d3d26417e2b68e
marcos <marcos@hzontal.org>
2020-12-03 12:16:27 -0300

    add cypress - e2e test suite

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line no-unused-vars
+const PluginConfig: Cypress.PluginConfig = (on, config): void => {
+  // Plugin configuration
+}
+
+export default PluginConfig
