8032324db11f9e366ecddfcf5e0988b5bbf27249
marcos <marcos@hzontal.org>
2020-12-07 13:52:46 -0300

    make some changes

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,5 +2,5 @@ import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
 
 storiesOf('Button', module)
-  .add('with text', () => <Button>Hello Button</Button>)
+  .add('with text', () => <Button>Hi</Button>)
   .add('with some emoji', () => <Button>😀 😎 👍 💯</Button>)
