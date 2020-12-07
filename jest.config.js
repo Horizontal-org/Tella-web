1b7fbb4b1a353c12a1e6c50a8ac99abeb82184b9
marcos <marcos@hzontal.org>
2020-12-07 12:37:09 -0300

    cypress and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress'],
   testMatch: ['**/*.(test|spec).(ts|tsx)'],
   moduleNameMapper: {
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
