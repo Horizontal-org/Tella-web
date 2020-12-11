00232c97ccb61582b19db39dc17649256a25a882
marcos <marcos@hzontal.org>
2020-12-11 12:01:47 -0300

    fix navButton style

--- components/NavButton/NavButton.tsx
+++ components/NavButton/NavButton.tsx
@@ -1,6 +1,4 @@
-import { FC, ReactNode } from 'react'
-
-import style from './NavButton.module.scss'
+import { FunctionComponent, ReactNode } from 'react'
 
 type Props = {
   text: string
@@ -9,13 +7,19 @@ type Props = {
   selected?: boolean
 }
 
-export const NavButton: FC<Props> = ({ text, icon, disabled, selected }: Props) => {
+const baseStyle =
+  'flex flex-row font-bold items-center h-10 px-4 rounded text-base bg-white text-gray-500'
+const enableStyle = 'hover:bg-black hover:bg-opacity-5 hover:text-gray-700 cursor-pointer'
+const disabledStyle = 'opacity-40 cursor-not-allowed'
+const selectedStyle = 'bg-blue-light text-blue-500'
+
+export const NavButton: FunctionComponent<Props> = ({ text, icon, disabled, selected }: Props) => {
   return (
     <div
       className={[
-        style.navButton,
-        disabled ? style.disabled : style.enabled,
-        selected ? style.selected : '',
+        baseStyle,
+        disabled ? disabledStyle : enableStyle,
+        selected ? selectedStyle : '',
       ].join(' ')}
     >
       <span className="flex items-center justify-center text-xl">{icon}</span>
