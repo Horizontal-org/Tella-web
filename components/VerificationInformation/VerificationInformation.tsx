1f3e2b037a144599f762977d308d76cd3438cf82
Ariel R-A <raarielgrace@gmail.com>
2021-02-12 18:09:55 -0500

    Added skeleton code for VI.

---
+++ components/VerificationInformation/VerificationInformation.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent } from 'react'
+
+type Props = {
+  file: FileObj,
+  device: DeviceObj,
+  env: EnvironmentObj
+}
+
+export const ReportInformation: FunctionComponent<Props> = ({ file, device, env }) => (
+  <>
+    <h3 className="text-base font-bold text-gray-500 py-3">Verification Information</h3>
+    <div className="">
+        <span>Information about the file</span>
+        {file.map((data) => (
+            <div>
+                <span>{data.title}</span>
+                <span>{data.value}</span>
+            </div>
+        ))}
+    </div>
+    <div className="">
+        <span>Information about the device</span>
+        {device.map((data) => (
+            <div>
+                <span>{data.title}</span>
+                <span>{data.value}</span>
+            </div>
+        ))}
+    </div>
+    <div className="">
+        <span>Information about the environment where the file was captured</span>
+        {env.map((data) => (
+            <div>
+                <span>{data.title}</span>
+                <span>{data.value}</span>
+            </div>
+        ))}
+    </div>
+  </>
+)
