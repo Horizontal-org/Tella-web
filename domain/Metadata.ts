0d58dcdbf1bc0f31ac8d83d3be0a7ac8779345e0
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2021-02-23 12:13:36 -0500

    For each subcategory of metadata, there is an interface with the information about that subcategory.

--- domain/Metadata.ts
+++ domain/Metadata.ts
@@ -1,13 +1,38 @@
 import ReactNode from 'react'
 
 export interface FileObj {
+  path?: string
+  hash?: int
+  date?: Date
 
 }
 
 export interface DeviceObj {
+  manufacturer?: string
+  hardware?: string
+  deviceId?: int
+  screenSize?: float
+  language?: string
+  locale?: string
+  connection?: string
+  networkType?: string
+  wifiMAC?: string
+  ipv4?: string
+  ipv6?: string
+}
 
+export interface Location {
+  latitude: float
+  longitude: float
+  altitude: float
+  accuracy: float
+  timestamp: int
 }
 
 export interface EnvironmentObj {
-
+  location?: Location
+  locationProvider?: string
+  speed?: float
+  cellInfo?: int[]
+  wifiInfo?: string[]
 }
