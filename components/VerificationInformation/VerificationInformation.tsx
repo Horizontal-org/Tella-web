ded9819f1073181178c0abaa979ab3841da645cd
Ariel R-A <raarielgrace@gmail.com>
2021-03-02 12:08:59 -0500

    Formatted VI component CSS.

--- components/VerificationInformation/VerificationInformation.tsx
+++ components/VerificationInformation/VerificationInformation.tsx
@@ -1,37 +1,52 @@
 import { FunctionComponent } from 'react'
-import {FileObj, DeviceObj, EnvironmentObj, Location} from '../../domain/Metadata'
+import { FileObj, DeviceObj, EnvironmentObj, Location } from '../../domain/Metadata'
 
 type Props = {
-  file?: FileObj,
-  device?: DeviceObj,
+  file?: FileObj
+  device?: DeviceObj
   env?: EnvironmentObj
 }
 
-export const VerificationInformation: FunctionComponent<Props> = ({ file, device, env }) => (
+export const VerificationInformation: FunctionComponent<Props> = ({
+  file = {},
+  device = {},
+  env = {},
+}) => (
   <>
-    <h3 className="text-base font-bold text-gray-500 py-3">Verification Information</h3>
-    <div className="">
-        <span>Information about the file</span>
-        {file && (Object.entries(file).map(([key, value]) =>
-          <div>
-              <span>{key}</span>: <span>{value}</span>
+    <h3 className="text-base font-bold text-gray-500 py-3">File Information</h3>
+    <div className="text-base font-bold text-gray-500">
+      <div className="py-3">
+        <span>File</span>
+      </div>
+      {file &&
+        Object.entries(file).map(([key, value]) => (
+          <div className="font-normal grid grid-cols-2 text-sm text-gray-500 gap-y-2 py-1">
+            <span>{key}</span>
+            <span className="text-black text-opacity-80">{value}</span>
           </div>
         ))}
     </div>
-    <div className="">
-        <span>Information about the device</span>
-        {device && (Object.entries(device).map(([key, value]) =>
-          <div>
-              <span>{key}</span>: <span>{value}</span>
+    <div className="text-base font-bold text-gray-500">
+      <div className="py-3">
+        <span>Device</span>
+      </div>
+      {file &&
+        Object.entries(device).map(([key, value]) => (
+          <div className="font-normal grid grid-cols-2 text-sm text-gray-500 gap-y-2 py-1">
+            <span>{key}</span>
+            <span className="text-black text-opacity-80">{value}</span>
           </div>
         ))}
     </div>
-    <div className="">
-        <span>Information about the environment where the file was captured</span>
-        {env && (Object.entries(env).map(([key, value]) =>
-          <div>
-              <span>{data.title}</span>
-              <span>{data.value}</span>
+    <div className="text-base font-bold text-gray-500">
+      <div className="py-3">
+        <span>Environment</span>
+      </div>
+      {file &&
+        Object.entries(env).map(([key, value]) => (
+          <div className="font-normal grid grid-cols-2 text-sm text-gray-500 gap-y-2 py-1">
+            <span>{key}</span>
+            <span className="text-black text-opacity-80">{value}</span>
           </div>
         ))}
     </div>
