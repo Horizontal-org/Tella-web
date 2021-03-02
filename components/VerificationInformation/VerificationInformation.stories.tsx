9746006b0109103b39ff2f429d3b9ff352bc926c
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2021-03-02 12:46:24 -0500

    Added moked data for metadata

--- components/VerificationInformation/VerificationInformation.stories.tsx
+++ components/VerificationInformation/VerificationInformation.stories.tsx
@@ -1,13 +1,13 @@
 import { storiesOf } from '@storybook/react'
 import { VerificationInformation } from './VerificationInformation'
 import {FileObj, DeviceObj, EnvironmentObj, Location} from '../../domain/Metadata'
+import { MetadataMokedData } from '../../moked/metadata'
 
 storiesOf('Verification Information', module).add('Verification Information', () => {
-  const testFile: FileObj = {path: "this_is_a_path", hash: 1234}
-  
+
   return (
     <div className="w-64 p-2">
-      <VerificationInformation file={testFile}/>
+      <VerificationInformation file={MetadataMokedData}/>
     </div>
   )
 })
