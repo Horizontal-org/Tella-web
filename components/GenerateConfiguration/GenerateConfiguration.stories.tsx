e17b068ba2319c67f8871762328988f6485aa5ba
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2021-04-22 19:12:30 -0400

    removing test QR generation page

--- components/GenerateConfiguration/GenerateConfiguration.stories.tsx
+++ components/GenerateConfiguration/GenerateConfiguration.stories.tsx
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/react'
 import { MokedConfiguration } from '../../moked/configuration'
 import { GenerateConfiguration } from './GenerateConfiguration'
 
-storiesOf('GenerateConfiguration', module).add('GenerateConfiguration', () => {
+storiesOf('Generate Configuration', module).add('Generate Configuration', () => {
   return (
     <div className="w-auto p-2">
       <GenerateConfiguration name="Test Configuration" configuration={MokedConfiguration} />
